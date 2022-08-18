e1383e1c033719638be18bb622ba81a11908c3a7
d3uk4li0n <99892625+d3uk4li0n@users.noreply.github.com>
2022-08-18 14:12:05 +0000

    Add files via upload

--- include/my_printf.h
+++ include/my_printf.h
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>
 
+#ifndef PRINTF_H
+#define PRINTF_H
+
+int my_printf(char *, ...);
+int my_strlen(const char *);
+void print_char(char);
+int absolute(int);
+void rev_string(char *, char *);
+char* my_strcpy(char *, char *);
+char handle_hex(int);
+void print_pointer(void *);
+char *my_itoa(int, char *, int);
+
+#endif
